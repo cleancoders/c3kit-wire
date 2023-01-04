c7d22eafb92c4e663c68d38e0419bfb3fdb09c27
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2023-01-04 17:47:49 -0500

    Add JWT

---
+++ spec/clj/c3kit/wire/jwt_spec.clj
@@ -0,0 +1,173 @@
+(ns c3kit.wire.jwt-spec
+  (:require [buddy.sign.jwt :as buddy-jwt]
+            [c3kit.apron.schema :as schema]
+            [c3kit.apron.time :as time]
+            [c3kit.wire.jwt :as sut]
+            [c3kit.wire.spec-helperc :as wire-helperc]
+            [ring.middleware.anti-forgery.strategy :as strategy]
+            [speclj.core :refer :all])
+  (:import (clojure.lang ExceptionInfo)))
+
+(def jwt-options {:cookie-name "sid-cookie" :secret "secret-sauce" :lifespan (time/minutes 30)})
+(def ensure-client-id (partial sut/ensure-client-id jwt-options))
+(def sign-response (partial sut/sign-response jwt-options))
+(def now (time/now))
+(def sid-payload {:id 123 :email "sid@sesamestreet.com"})
+(def strategy (sut/create-strategy))
+(def valid-token? (partial strategy/valid-token? strategy))
+(def get-token (partial strategy/get-token strategy))
+(def write-token (partial strategy/write-token strategy))
+
+(describe "JWT"
+  (with-stubs)
+  (wire-helperc/stub-now now)
+
+  (it "wraps a handler"
+    (let [handle      (sut/wrap-jwt identity jwt-options)
+          exp         (time/seconds-since-epoch (time/after now (time/minutes 10)))
+          payload     (buddy-jwt/sign (assoc sid-payload :exp exp) "secret-sauce")
+          response    (handle {:cookies {"sid-cookie" {:value payload}}})
+          new-payload (assoc sid-payload :iat (time/seconds-since-epoch now) :exp (time/seconds-since-epoch (time/after now (time/minutes 30))))
+          cookie      (get-in response [:cookies "sid-cookie" :value])]
+      (should= new-payload (:jwt/payload response))
+      (should= new-payload (buddy-jwt/unsign (:jwt/token response) "secret-sauce"))
+      (should= new-payload (buddy-jwt/unsign cookie "secret-sauce"))))
+
+  (it "copy-payload"
+    (should= {:jwt/payload nil} (sut/copy-payload {} {}))
+    (should= {:jwt/payload nil} (sut/copy-payload {:jwt/payload {:foo :bar}} {}))
+    (should= {:jwt/payload {:foo :bar}} (sut/copy-payload {} {:jwt/payload {:foo :bar}}))
+    (should= {:jwt/payload {:foo :bar}} (sut/copy-payload {:jwt/payload {:bar :foo}} {:jwt/payload {:foo :bar}})))
+
+  (it "update-payload"
+    (should= {:jwt/payload {:foo :bar}} (sut/update-payload {} assoc :foo :bar))
+    (should= {:jwt/payload "foo"} (sut/update-payload {:jwt/payload :foo} name))
+    (should= {:jwt/payload 6} (sut/update-payload {:jwt/payload 0} + 1 2 3)))
+
+  (context "unsign!"
+    (it "a missing exp claim mimics buddy-signs behavior with an expired exp claim"
+      (let [secret "secret-sauce"
+            token  (buddy-jwt/sign sid-payload secret)]
+        (try
+          (sut/unsign! token secret)
+          (should-fail "Expected unsign! to throw")
+          (catch ExceptionInfo e
+            (should= "Token is expired" (ex-message e))
+            (should= {:type :validation :cause :exp} (ex-data e))))))
+
+    (it "expired exp claim"
+      (let [secret  "secret-sauce"
+            exp     (time/seconds-since-epoch (time/before now (time/minutes 5)))
+            payload (assoc sid-payload :exp exp)
+            token   (buddy-jwt/sign payload secret)]
+        (should-throw ExceptionInfo (str "Token is expired (" exp ")") (sut/unsign! token secret))))
+
+    (it "unexpired exp claim"
+      (let [secret  "secret-sauce"
+            exp     (time/seconds-since-epoch (time/after now (time/minutes 5)))
+            payload (assoc sid-payload :exp exp)
+            token   (buddy-jwt/sign payload secret)]
+        (should= payload (sut/unsign! token secret))))
+    )
+
+  (context "ensuring client ids in requests"
+    (it "missing token"
+      (-> {} ensure-client-id :jwt/payload :client-id parse-uuid should))
+
+    (it "malformed token"
+      (let [response (ensure-client-id {:cookies {"sid-cookie" {:value "bad token"}}})]
+        (should= {"sid-cookie" {:value "bad token"}} (:cookies response))
+        (should= "bad token" (:jwt/token response))
+        (-> response :jwt/payload :client-id parse-uuid should)))
+
+    (it "empty map in payload"
+      (let [token    (buddy-jwt/sign {} "secret-sauce")
+            response (ensure-client-id {:cookies {"sid-cookie" {:value token}}})]
+        (should= token (get-in response [:cookies "sid-cookie" :value]))
+        (should= token (:jwt/token response))
+        (-> response :jwt/payload :client-id parse-uuid should)))
+
+    (it "payload missing exp"
+      (let [token    (buddy-jwt/sign sid-payload "secret-sauce")
+            response (ensure-client-id {:cookies {"sid-cookie" {:value token}}})]
+        (should= token (get-in response [:cookies "sid-cookie" :value]))
+        (should= token (:jwt/token response))
+        (-> response :jwt/payload :client-id parse-uuid should)))
+
+    (it "user data in payload"
+      (let [payload  (assoc sid-payload :exp (time/seconds-since-epoch (time/after now (time/minutes 10))))
+            token    (buddy-jwt/sign payload "secret-sauce")
+            response (ensure-client-id {:cookies {"sid-cookie" {:value token}}})]
+        (should= token (get-in response [:cookies "sid-cookie" :value]))
+        (should= token (:jwt/token response))
+        (should= payload (:jwt/payload response)))))
+
+  (context "signing responses"
+    (it "missing jwt/payload in request and response"
+      (let [response (sign-response {} {})]
+        (should-not (:jwt/payload response))))
+
+    (it "missing jwt/payload in response"
+      (let [response (sign-response {:jwt/payload {:client-id "123"}} {})
+            iat      (time/seconds-since-epoch now)
+            exp      (time/seconds-since-epoch (time/after now (time/minutes 30)))
+            {:keys [value secure path]} (get-in response [:cookies "sid-cookie"])]
+        (should= (buddy-jwt/sign {:client-id "123" :iat iat :exp exp} "secret-sauce") value)
+        (should secure)
+        (should= "/" path)))
+
+    (it "contains jwt/payload in response"
+      (let [response (sign-response {} {:jwt/payload sid-payload})
+            iat      (time/seconds-since-epoch now)
+            exp      (time/seconds-since-epoch (time/after now (time/minutes 30)))
+            token    (get-in response [:cookies "sid-cookie" :value])]
+        (should= (assoc sid-payload :iat iat :exp exp) (:jwt/payload response))
+        (should= (buddy-jwt/unsign token "secret-sauce") (:jwt/payload response))))
+
+    (it "does not specify a lifespan"
+      (let [sign-response (partial sut/sign-response (dissoc jwt-options :lifespan))
+            response      (sign-response {} {:jwt/payload sid-payload})
+            iat           (time/seconds-since-epoch now)
+            exp           (time/seconds-since-epoch (time/after now (time/hours 1)))
+            token         (get-in response [:cookies "sid-cookie" :value])]
+        (should= (assoc sid-payload :iat iat :exp exp) (:jwt/payload response))
+        (should= (buddy-jwt/unsign token "secret-sauce") (:jwt/payload response)))))
+
+  (context "get-token"
+    (it "retrieves the client id from the JWT payload"
+      (should= "client-id" (get-token {:jwt/payload {:client-id "client-id"}}))
+      (should= "something else" (get-token {:jwt/payload {:client-id "something else"}})))
+
+    (it "generates a uuid when there is no client id"
+      (let [token (get-token {})]
+        (schema/->uuid token)
+        (should (string? token)))))
+
+  (context "valid-token?"
+    (it "invalid when there is no client id"
+      (should-not (valid-token? {} "token"))
+      (should-not (valid-token? {:jwt/payload {:client-id nil}} "token")))
+
+    (it "invalid when the token doesn't match what's expected"
+      (should-not (valid-token? {:jwt/payload {:client-id "fake"}} "token")))
+
+    (it "valid when the token matches what's expected"
+      (should (valid-token? {:jwt/payload {:client-id "token"}} "token"))
+      (should (valid-token? {:jwt/payload {:client-id "abc123"}} "abc123"))))
+
+  (context "write-token"
+    (it "does nothing when the token matches the request"
+      (let [request  {:jwt/payload {:client-id "abc123"}}
+            response (write-token request {} "abc123")]
+        (should= {} response)))
+
+    (it "adds the token to the response when it doesn't match the request"
+      (let [request  {:jwt/payload {:user-id 123 :client-id "123abc"}}
+            response (write-token request {} "abc123")]
+        (should= {:jwt/payload {:user-id 123 :client-id "abc123"}} response)))
+
+    (it "does not overwrite the jwt payload if it already exists"
+      (let [request  {:jwt/payload {:user-id 123 :client-id "123abc"}}
+            response (write-token request {:jwt/payload {:user-id 1}} "abc123")]
+        (should= {:jwt/payload {:user-id 1 :client-id "abc123"}} response))))
+  )
---
+++ src/clj/c3kit/wire/jwt.clj
@@ -0,0 +1,123 @@
+(ns c3kit.wire.jwt
+  (:require [buddy.sign.jwt :as buddy-jwt]
+            [c3kit.apron.corec :as ccc]
+            [c3kit.apron.time :as time]
+            [ring.middleware.anti-forgery.strategy :as strategy]))
+
+(def new-client-id
+  "Generate a new client id."
+  (ccc/narity (comp str ccc/new-uuid)))
+
+(def client-id
+  "Read the client id from a request."
+  (comp :client-id :jwt/payload))
+
+(defn- some-payload [& es] (some :jwt/payload es))
+
+(defn copy-payload
+  "Copy the JWT Payload from second parameter to the first."
+  [to from]
+  (assoc to :jwt/payload (:jwt/payload from)))
+
+(defn update-payload
+  "Applies the update function to the JWT Payload."
+  [m f & args]
+  (apply update m :jwt/payload f args))
+
+(defn sign
+  "Create a signed JWT token with the given claims.
+   :iat and :exp claims are added will override any
+   pre-existing :iat and :exp claims."
+  [claims secret lifespan]
+  (let [iat (time/seconds-since-epoch (time/now))
+        exp (+ iat (time/millis->seconds (or lifespan (time/hours 1))))]
+    (-> claims
+        (assoc :iat iat)
+        (assoc :exp exp)
+        (buddy-jwt/sign secret))))
+
+(defn unsign!
+  "Read the payload from a JWT token.
+   Throw an exception if the token is expired or if the signature is invalid."
+  [token secret]
+  (let [payload (buddy-jwt/unsign token secret)]
+    (if (:exp payload)
+      payload
+      (throw (ex-info "Token is expired" {:type :validation :cause :exp})))))
+
+(defn unsign
+  "Read the payload from a JWT token.
+   Return nil if the token is expired or if the signature is invalid."
+  [token secret]
+  (try
+    (unsign! token secret)
+    (catch Exception _)))
+
+(defn- unsign-or-default [token secret]
+  (or (unsign token secret)
+      {:client-id (new-client-id)}))
+
+(defn ensure-client-id
+  "Decode the JWT Payload into the request
+   and ensure the payload contains a client id."
+  [{:keys [cookie-name secret]} request]
+  (if-let [token (get-in request [:cookies cookie-name :value])]
+    (-> request
+        (assoc :jwt/payload (unsign-or-default token secret))
+        (assoc :jwt/token token))
+    (assoc-in request [:jwt/payload :client-id] (new-client-id))))
+
+(defn sign-response
+  "If either the response or request contains a :jwt/payload,
+   generate a JWT token and add it to response's cookies."
+  [{:keys [cookie-name secret lifespan]} request response]
+  (if-let [payload (some-payload response request)]
+    (let [token (sign payload secret lifespan)]
+      (-> response
+          (assoc :jwt/payload (unsign! token secret))
+          (assoc :jwt/token token)
+          (assoc-in [:cookies cookie-name] {:value token :secure true :path "/"})))
+    response))
+
+(defn wrap-jwt
+  "Options:
+    :cookie-name - The name of the browser cookie that the JWT is stored in.
+    :secret - The secret used to sign and read the JWT token.
+    :lifespan - The time in milliseconds that the token is alive for. (default 3600000/1hr)"
+  [handler options]
+  (fn [request]
+    (let [request (ensure-client-id options request)]
+      (->> request
+           handler
+           (sign-response options request)))))
+
+(def get-token
+  "Get the client id from the request or generate a new client id."
+  (some-fn client-id new-client-id))
+
+(defn valid-token?
+  "True if the token matches the client id in the request."
+  [request token]
+  (some-> request client-id (= token)))
+
+(defn write-token
+  "Return the unmodified response if the token matches the request's client id.
+   Otherwise, assign the token to the response's client id."
+  [request response token]
+  (if (= (client-id request) token)
+    response
+    (-> response
+        (assoc :jwt/payload (some-payload response request))
+        (assoc-in [:jwt/payload :client-id] token))))
+
+; Because the anti-forgery strategy does not sign/unsign the :jwt/token,
+; the anti-forgery handler must be wrapped by the wrap-handler (above)
+(deftype JWTStrategy []
+  strategy/Strategy
+  (get-token [_ request] (get-token request))
+  (valid-token? [_ request token] (valid-token? request token))
+  (write-token [_ request response token] (write-token request response token)))
+
+(def create-strategy
+  "Synonym for ->JWTStrategy"
+  ->JWTStrategy)
