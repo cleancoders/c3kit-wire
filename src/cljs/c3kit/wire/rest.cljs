583b4eb865feae6827bc027a2ec4e7665265b281
Alex Jensen <alex@endure.io>
2025-10-29 15:17:49 -0400

    keeps track of active HTTP requests

--- src/cljs/c3kit/wire/rest.cljs
+++ src/cljs/c3kit/wire/rest.cljs
@@ -2,30 +2,32 @@
   (:require [c3kit.wire.restc :as restc]
             [cljs-http.client :as client]
             [cljs.core.async :refer-macros [go]]
-            [cljs.core.async :as async]))
+            [cljs.core.async :as async]
+            [reagent.core :as reagent]))
+
+(def active-reqs (reagent/atom 0))
+(defn activity? [] (not= 0 @active-reqs))
 
 (defn- async-callback! [channel callback]
   ;; [GMJ] `go` block is not tested because I can't get it to work with speclj
   (go
-    (callback (async/<! channel)))
+    (swap! active-reqs inc)
+    (callback (async/<! channel))
+    (swap! active-reqs dec))
   nil)
 
-(defn request! [method url request]
-  ;; [GMJ] had to extract this to make specs pass with advanced optimization for some reason...
-  (method url (restc/-maybe-update-req request)))
+(defn request! [method url request callback]
+  (let [channel (method url (restc/-maybe-update-req request))]
+    (async-callback! channel callback)))
 
 (defn get! [url request callback]
-  (let [channel (request! client/get url request)]
-    (async-callback! channel callback)))
+  (request! client/get url request callback))
 
 (defn post! [url request callback]
-  (let [channel (request! client/post url request)]
-    (async-callback! channel callback)))
+  (request! client/post url request callback))
 
 (defn put! [url request callback]
-  (let [channel (request! client/put url request)]
-    (async-callback! channel callback)))
+  (request! client/put url request callback))
 
 (defn delete! [url request callback]
-  (let [channel (request! client/delete url request)]
-    (async-callback! channel callback)))
+  (request! client/delete url request callback))
