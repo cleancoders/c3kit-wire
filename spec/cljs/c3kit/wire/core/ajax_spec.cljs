3b11b87ad659f9cb2e51e00397bebd2eeb70ca0b
Alex Root-Roatch <65352758+arootroatch@users.noreply.github.com>
2026-04-27 18:49:07 -0500

    wire-split: cover core.ajax default-state and activity?

--- spec/cljs/c3kit/wire/core/ajax_spec.cljs
+++ spec/cljs/c3kit/wire/core/ajax_spec.cljs
@@ -15,4 +15,14 @@
           my-atom-fn (fn [v] (swap! calls conj v) (atom v))
           s (sut/make-state my-atom-fn)]
       (should= [0] @calls)
-      (should= 0 @(:active-requests s)))))
+      (should= 0 @(:active-requests s))))
+
+  (it "activity? reflects default-state's active-requests counter"
+    (should= false (sut/activity?))
+    (swap! sut/active-ajax-requests inc)
+    (should= true (sut/activity?))
+    (swap! sut/active-ajax-requests dec)
+    (should= false (sut/activity?)))
+
+  (it "default-state shares its active-requests atom with the active-ajax-requests def"
+    (should= sut/active-ajax-requests (:active-requests sut/default-state))))
