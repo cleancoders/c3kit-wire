78424ff2a4f4bf899b521d25daac783f8f67c8eb
Gina <maniginam@gmail.com>
2026-06-25 19:13:53 -0500

    feat: add secure-by-default cacheable? predicate

--- spec/cljs/c3kit/wire/service_worker_spec.cljs
+++ spec/cljs/c3kit/wire/service_worker_spec.cljs
@@ -1,6 +1,7 @@
 (ns c3kit.wire.service-worker-spec
   (:require-macros [speclj.core :refer [context describe it should should= should-be-nil should-not should-contain]])
   (:require [c3kit.wire.service-worker-fake :as fake]
+            [c3kit.wire.service-worker :as sut]
             [speclj.core]))
 
 (defn resolved-value [thenable]
@@ -44,3 +45,39 @@
                 (let [caught (atom nil)]
                   (.catch ((fake/fake-fetch :reject) req) #(reset! caught %))
                   (should= true (instance? js/Error @caught))))))
+
+(describe "cacheable?"
+          (let [ctx (fake/->ctx {:scope (fake/->scope {:origin "https://app.test"})})
+                get-req (fake/->request "https://app.test/img.png")
+                ok-resp (fake/->response "x")]
+
+            (it "true for same-origin ok GET"
+                (should= true (sut/cacheable? ctx get-req ok-resp {})))
+
+            (it "false for non-GET"
+                (should= false (sut/cacheable? ctx (fake/->request "https://app.test/x" {:method "POST"}) ok-resp {})))
+
+            (it "false for non-ok response"
+                (should= false (sut/cacheable? ctx get-req (fake/->response "x" {:ok false}) {})))
+
+            (it "false for opaque response"
+                (should= false (sut/cacheable? ctx get-req (fake/->response "x" {:type "opaque"}) {})))
+
+            (it "false for opaqueredirect response"
+                (should= false (sut/cacheable? ctx get-req (fake/->response "x" {:type "opaqueredirect"}) {})))
+
+            (it "false for no-store response"
+                (should= false (sut/cacheable? ctx get-req (fake/->response "x" {:headers {"Cache-Control" "no-store"}}) {})))
+
+            (it "false for cross-origin by default, true when allowed"
+                (let [x (fake/->request "https://cdn.other/x.png")]
+                  (should= false (sut/cacheable? ctx x ok-resp {}))
+                  (should= true  (sut/cacheable? ctx x ok-resp {:allow-cross-origin true}))))
+
+            (it "false for credentialed by default, true when allowed"
+                (let [c (fake/->request "https://app.test/x" {:credentials "include"})
+                      a (fake/->request "https://app.test/x" {:headers {"Authorization" "Bearer t"}})]
+                  (should= false (sut/cacheable? ctx c ok-resp {}))
+                  (should= false (sut/cacheable? ctx a ok-resp {}))
+                  (should= true  (sut/cacheable? ctx c ok-resp {:cache-credentialed true}))
+                  (should= true  (sut/cacheable? ctx a ok-resp {:cache-credentialed true}))))))
